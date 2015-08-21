02e6f3e796e8ed30b5602b1173ead6725bfb38ff
unknown <liesislukas@gmail.com>
2015-08-22 01:12:06 +0300

    express server to get position on google

---
+++ examples/seo-tool/server/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+	if(!req.query.keyword || !req.query.domain){
+		res.json({
+			error: 'missing params. Add keyword and domain to query'
+		});
+	}
+	
+	function get_google_position(keyword, domain, start){
+		if(!keyword || !domain){
+			return;
+		}
+		var request = require('request');
+		var page = 0;
+		var url = 'https://www.google.com/search?q='+keyword+'&start='+start;
+			
+		request.get(url, function (error, response, body) {
+		    if (!error && response.statusCode == 200) {
+		    	var index = body.indexOf(domain);
+				start = start + 10;
+
+				if(index < 0){
+					if(start <= 100){
+						page = get_google_position(keyword, domain, start)
+					} else {
+						res.json({page: -1});
+					}
+				} else {
+					page = start / 10;
+					res.json({page: page});
+				}
+		    }
+		});
+
+		return page;
+	}	
+
+	get_google_position(req.query.keyword, req.query.domain, 0);
+ 	
+});
+
+app.listen(3333, function(){
+	console.log('listening on 3333');
+});
