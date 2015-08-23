d2e5b875a59ac78b56dde8d218fa72db405d3e82
unknown <liesislukas@gmail.com>
2015-08-23 16:12:11 +0300

    handle form to state

--- examples/react-express/app/components/GroceryListAddItem.jsx
+++ examples/react-express/app/components/GroceryListAddItem.jsx
@@ -1,11 +1,25 @@
 var React = require('react');
 
 module.exports = React.createClass({
+	getInitialState: function() {
+		return {
+			input: ""
+		};
+	},
+	handleInputName: function(e){
+		this.setState({
+			input: e.target.value
+		});
+	},
+	addItem: function(e){
+		e.preventDefault();
+		console.log('addItem ',this.state.input);
+	},
 	render: function(){
 		return (
 			<div className="grocery-AddItem">
-				<form>
-					<input type="text"/>
+				<form onSubmit={this.addItem}>
+					<input type="text" value={this.state.input} onChange={this.handleInputName}/>
 					<button>Add Item</button>
 				</form>
 			</div>
