31234d086c0e23793b5d7737167ac082c342b5c0
unknown <liesislukas@gmail.com>
2015-08-23 15:36:13 +0300

    gulp & browserify & reactify

--- examples/react-express/gulpfile.js
+++ examples/react-express/gulpfile.js
@@ -1,15 +1,29 @@
 var gulp = require('gulp');
 var gls = require('gulp-live-server');
 var browserSync = require('browser-sync');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
 
 gulp.task('live-server', function(){
 	var server = gls('./server/main.js');
 	server.start();
 });
 
+gulp.task('bundle', function(){
+	return browserify({
+		entries: 'app/main.jsx',
+		debug: true
+	})
+	.transform(reactify)
+	.bundle()
+	.pipe(source('app.js'))
+	.pipe(gulp.dest('./.tmp'));
+});
+
 gulp.task('serve', ['live-server'], function(){
 	browserSync.init(null, {
 		proxy: 'http://localhost:7777/',
 		port: 9001
-	}); 
+	});  
 });
--- examples/react-express/server/main.js
+++ examples/react-express/server/main.js
@@ -4,6 +4,7 @@ var app = new express();
 app.get('/', function(req, res){
 	res.render('./../app/index.ejs', {});
 })
+.use(express.static(__dirname + '/../.tmp'))
 .listen(7777, function(){
 	console.log('listening on 7777');
 });
